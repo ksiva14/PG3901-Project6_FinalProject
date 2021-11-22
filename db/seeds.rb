ef00b9e7ea613de5dbad6217ccbcb745eaf2cc14
Zheng Ji <tan.955@osu.edu>
2021-11-22 13:42:56 -0500

    added seeds to populate database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+30.times do
+  User.create! name: Faker::Name.unique.name, email: Faker::Internet.unique.email,
+               password_digest: Faker::Alphanumeric.alphanumeric(number: 10)
+end
+
+num = 3901
+index = 1
+5.times do
+  Team.create! team_name: Faker::Game.unique.title
+  Course.create! course_num: num
+  Project.create! project_name: `Project #{index}`
+  num += 1
+  index += 1
+end

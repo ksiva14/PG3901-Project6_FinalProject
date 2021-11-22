318985803b71b7ac2b207cd2838a4baf635518ad
Zheng Ji <tan.955@osu.edu>
2021-11-21 21:28:17 -0500

    added static_pages controller

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get evaluation" do
+    get evaluation_path
+    assert_response :success
+  end
+end

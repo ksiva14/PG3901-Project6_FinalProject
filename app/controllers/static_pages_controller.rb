72459ffcad5408d88bcd44704d8fd17adc015827
Zheng Ji <tan.955@osu.edu>
2021-11-22 00:31:43 -0500

    added grading scale description

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,40 @@
 class StaticPagesController < ApplicationController
+  
   def evaluation
+    grading_scale = [
+      {
+        grade: '0-3',
+        title: 'Poor team-mate',
+        description: ['Did not contribute to the final product', 'Put forth little or insufficient effort',
+                      'Was rarely present at team meetings and was difficult to get a hold of',
+                      'You would **avoid partnering** with this person in a future course']
+      }, {
+        grade: '3-4',
+        title: 'Fair team-mate',
+        description: ['Worked hard at contributing to the project', 'Struggled to carry out assigned responsibilities',
+                      'Made minor contributions to the final product', 'You would **prefer not to partner** with this person in a future course']
+      }, {
+        grade: '5-6',
+        title: 'Good team-mate',
+        description: ['Carried out their assigned responsibilities', 'Contributed to the final product',
+                      'Room for improvement in communication, initiative, effort, or skills',
+                      "You would be **willing to partner** with them in a future course, though you wouldn't seek them out"]
+      }, {
+        grade: '7-8',
+        title: 'Very good team-mate',
+        description: ['Contributed significantly', 'Communicated effectively',
+                      'Took initiative in the design and implementation',
+                      'You would **seek out** this person to be a team-mate in a future course']
+      }, {
+        grade: '9-10',
+        title: 'Amazing team-mate',
+        description: ['You will be in contact with them for years',
+                      'One of the **best** team-mates you have ever had for any project']
+      }
+    ]
+
+    respond_to do |format|
+      format.html { render :evaluation, locals: { grading_scale: grading_scale } }
+    end
   end
 end

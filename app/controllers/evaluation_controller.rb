bb927447d0ad4e1fd31a284064e1d948a899c3aa
Zheng Ji <tan.955@osu.edu>
2021-11-21 21:13:51 -0500

    removed evaluation controller

--- app/controllers/evaluation_controller.rb
+++
@@ -1,4 +0,0 @@
-class EvaluationController < ApplicationController
-  def evaluation_form
-  end
-end
--- test/controllers/evaluation_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class EvaluationControllerTest < ActionDispatch::IntegrationTest
-  test 'should get evaluation_form' do
-    get evaluation_form_path
-    assert_response :success
-  end
-end

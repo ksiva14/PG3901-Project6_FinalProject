369a83a50907a6085e65bd96f0177f4f3b9632c9
Zheng Ji <tan.955@osu.edu>
2021-11-26 13:11:53 -0500

    fixed errors in fixture

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -2,4 +2,8 @@ class Evaluation < ApplicationRecord
   belongs_to :project
   belongs_to :team
   belongs_to :student
+
+  # validates_associated :project
+  # validates_associated :team
+  # validates_associated :student
 end

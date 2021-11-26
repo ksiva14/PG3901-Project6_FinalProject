476d8d4a844b8a7896e2c8da67ed82d204f161d2
Zheng Ji <tan.955@osu.edu>
2021-11-26 14:20:05 -0500

    added basic tests to models

--- test/models/evaluation_test.rb
+++ test/models/evaluation_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class EvaluationTest < ActiveSupport::TestCase
   def setup
-    @evaluation = Evaluation.new(project_id: 1, student_id: 10, team_id: 1, score: nil, comment: nil)
+    @evaluation = Evaluation.new(project_id: 1, student_id: 1, team_id: 1, score: nil, comment: nil)
   end
 
   test 'should be valid' do

cc4641baf0e27457d40f2b7df21576597c9cf057
jking3019 <justink101000@yahoo.com>
2021-11-26 14:49:20 -0500

    working on dashboard, still confused on where to put things

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   # resources :user
   # get '/evaluation', to: 'static_pages#evaluation'
   # # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'static_pages#evaluation'
+  root 'static_pages#evaluation',
+  page: 'dashboard'
 end

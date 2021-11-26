a990c1aaa8366c2e70ee46360083ac488c4050d9
Zheng Ji <tan.955@osu.edu>
2021-11-26 15:05:33 -0500

    added validation for user password

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_OSU_EMAIL }, uniqueness: true
-  validates :password_digest, presence: true, length: { minimum: 6 }
   has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new name: 'name', email: 'name.1@osu.edu', password_digest: 'password'
+    @user = User.new name: 'name', email: 'name.1@osu.edu', password: 'foobar', password_confirmation: 'foobar'
   end
 
   test 'should be valid' do
@@ -53,13 +53,13 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
 
-  # test 'password should be present (nonblank)' do
-  #   @user.password = @user.password_confirmation = ' ' * 6
-  #   assert_not @user.valid?
-  # end
+  test 'password should be present (nonblank)' do
+    @user.password = @user.password_confirmation = ' ' * 6
+    assert_not @user.valid?
+  end
 
-  # test 'password should have a minimum length' do
-  #   @user.password = @user.password_confirmation = 'a' * 5
-  #   assert_not @user.valid?
-  # end
+  test 'password should have a minimum length' do
+    @user.password = @user.password_confirmation = 'a' * 5
+    assert_not @user.valid?
+  end
 end

df1b18b743246de576f6ae35d9c8dadba7ab5488
Tyler <tylerfrantz65@gmail.com>
2021-11-30 12:00:45 -0500

    Added project files

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,45 @@
+class ProjectsController < ApplicationController
+  before_action :set_project, only: %i[show edit update destroy]
+
+  # GET /projects
+  # GET /projects.json
+  def index
+    @projects = Project.all
+  end
+
+  # POST /projects
+  # POST /projects.json
+  def create
+    @project = Project.new(project_params)
+
+    respond_to do |format|
+      if @project.save
+        format.html { redirect_to projects_url, notice: 'Project was successfully created.' }
+        # format.json { render :show, status: :created, location: @project }
+      end
+    end
+  end
+
+  # DELETE /projects/1
+  # DELETE /projects/1.json
+  def destroy
+    @project.destroy
+    respond_to do |format|
+      format.html { redirect_to projects_url, notice: 'Project was successfully destroyed.' }
+      # format.json { head :no_content }
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_project
+    @project = Project.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def project_params
+    # params.fetch(:project, {})
+    params.require(:project).permit(:project_name, :course_id)
+  end
+end

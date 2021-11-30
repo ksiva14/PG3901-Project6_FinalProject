ce3e7af7119bd3e3bea628022693e964da399f55
Zheng Ji <tan.955@osu.edu>
2021-11-30 00:25:54 -0500

    able to access student dashboard without being added to course

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,23 +1,22 @@
 class SessionsController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
+    # creates a student that is not added to any course
+    Student.create user_id: user.id
     if user && user.authenticate(params[:session][:password])
       log_in user
-      #Check if user is student
+      # Check if user is student
       if Student.find_by(user_id: user.id)
-        #Route to Students Dashboard
+        # Route to Students Dashboard
         redirect_to student_dashboard_path
       elsif Professor.find_by(user_id: user.id)
-        #Else Route to Professors Page
-        #redirect_to students_path
-      else
-        #redirect_to 
+        # Else Route to Professors Page
+        # redirect_to students_path
       end
-    else 
-      #Display Invalid Flash Message
+    else
+      # Display Invalid Flash Message
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
@@ -27,5 +26,4 @@ class SessionsController < ApplicationController
     log_out
     redirect_to root_url
   end
-
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,10 @@
 class UsersController < ApplicationController
-  #before_action :set_student, only: [:show, :edit, :update, :destroy]
+  # before_action :set_student, only: [:show, :edit, :update, :destroy]
 
   # GET /students/new
   def newStudent
     @student = User.new
   end
-
 
   def newProfessor
     @professor = User.new
@@ -16,34 +15,33 @@ class UsersController < ApplicationController
   def createStudent
     @student = User.new(user_params)
     if @student.save
-        flash[:success] = "Welcome: Login to Evaluation App!"
-        redirect_to root_path
+      flash[:success] = 'Welcome: Login to Evaluation App!'
+      redirect_to root_path
     else
-        render 'newStudent'
+      render 'newStudent'
     end
   end
 
   def createProfessor
     @professor = User.new(user_params)
     if @professor.save
-        #Save professor details in professor table
-        @prof = Professor.new
-        @prof.user_id = @professor.id
-        @prof.course_id = -1
-        @prof.save
-        #redirect
-        flash[:success] = "Welcome: Login to Evaluation App!"
-        redirect_to root_path
+      # Save professor details in professor table
+      @prof = Professor.new
+      @prof.user_id = @professor.id
+      @prof.course_id = -1
+      @prof.save
+      # redirect
+      flash[:success] = 'Welcome: Login to Evaluation App!'
+      redirect_to root_path
     else
-        render 'newProfessor'
+      render 'newProfessor'
     end
   end
 
   private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
-    
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
 end

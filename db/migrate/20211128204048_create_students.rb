d9dea5690feb34450e4546647aef8d481fe91727
Zheng Ji <tan.955@osu.edu>
2021-12-01 10:48:08 -0500

    removed dept_name from course as it is not in use

---
+++ db/migrate/20211128204048_create_students.rb
@@ -0,0 +1,10 @@
+class CreateStudents < ActiveRecord::Migration[6.1]
+  def change
+    create_table :students do |t|
+      t.references  :user, index: true
+      t.references  :team, index: true
+
+      t.timestamps
+    end
+  end
+end

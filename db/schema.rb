40edb06dfe1bdfdfb5070af207f8572a558709cc
jking3019 <justink101000@yahoo.com>
2021-12-05 02:39:54 -0500

    db create

--- db/schema.rb
+++ db/schema.rb
@@ -17,19 +17,19 @@ ActiveRecord::Schema.define(version: 2021_12_03_030609) do
 
   create_table "courses", force: :cascade do |t|
     t.integer "course_num"
-    t.string "course_name"
+    t.text "course_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "evaluations", force: :cascade do |t|
-    t.integer "project_id"
-    t.integer "team_id"
+    t.bigint "project_id"
+    t.bigint "team_id"
     t.integer "for_student"
     t.integer "by_student"
     t.boolean "by_professor"
     t.integer "score"
-    t.string "comment"
+    t.text "comment"
     t.boolean "is_assigned"
     t.boolean "can_view"
     t.datetime "created_at", precision: 6, null: false
@@ -39,8 +39,8 @@ ActiveRecord::Schema.define(version: 2021_12_03_030609) do
   end
 
   create_table "professors", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "course_id"
+    t.bigint "user_id"
+    t.bigint "course_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["course_id"], name: "index_professors_on_course_id"
@@ -55,16 +55,16 @@ ActiveRecord::Schema.define(version: 2021_12_03_030609) do
   end
 
   create_table "projects", force: :cascade do |t|
-    t.string "project_name"
-    t.integer "course_id"
+    t.text "project_name"
+    t.bigint "course_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["course_id"], name: "index_projects_on_course_id"
   end
 
   create_table "students", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "team_id"
+    t.bigint "user_id"
+    t.bigint "team_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["team_id"], name: "index_students_on_team_id"
@@ -72,17 +72,17 @@ ActiveRecord::Schema.define(version: 2021_12_03_030609) do
   end
 
   create_table "teams", force: :cascade do |t|
-    t.string "team_name"
-    t.integer "course_id"
+    t.text "team_name"
+    t.bigint "course_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["course_id"], name: "index_teams_on_course_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password_digest"
+    t.text "name"
+    t.text "email"
+    t.text "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
